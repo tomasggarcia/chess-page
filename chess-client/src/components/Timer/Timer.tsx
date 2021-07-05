f28a72b19a6283f3b2b49458de7412f170996358
Jupaolivera <75101920+Jupaolivera@users.noreply.github.com>
2021-07-05 18:19:57 -0300

    timer turn and display position fixes

--- chess-client/src/components/Timer/Timer.tsx
+++ chess-client/src/components/Timer/Timer.tsx
@@ -1,65 +1,63 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 
-import './Timer.css';
-
+import "./Timer.css";
 
 function Timer() {
-
-
-    const [timer, setTimer] = useState(60)
-    const [isActive, setIsActive] = useState(false)
-    const [isPaused, setIsPaused] = useState(false)
-    const increment = useRef<any>()
+    const [timer, setTimer] = useState(60);
+    const [isActive, setIsActive] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
+    const increment = useRef<any>();
 
     const handleStart = () => {
-        setIsActive(true)
-        setIsPaused(true)
+        setIsActive(true);
+        setIsPaused(true);
         increment.current = setInterval(() => {
-            setTimer((timer) => timer + 1)
-        }, 1000)
-    }
+            setTimer((timer) => timer - 1);
+        }, 1000);
+    };
 
     const handlePause = () => {
-        clearInterval(increment.current)
-        setIsPaused(false)
-    }
+        clearInterval(increment.current);
+        setIsPaused(false);
+    };
 
     const handleResume = () => {
-        setIsPaused(true)
+        setIsPaused(true);
         increment.current = setInterval(() => {
-            setTimer((timer) => timer + 1)
-        }, 1000)
-    }
+            setTimer((timer) => timer - 1);
+        }, 1000);
+    };
 
     const handleReset = () => {
-        clearInterval(increment.current)
-        setIsActive(false)
-        setIsPaused(false)
-        setTimer(0)
-    }
+        clearInterval(increment.current);
+        setIsActive(false);
+        setIsPaused(false);
+        setTimer(60);
+    };
 
     const formatTime = () => {
-        const getSeconds = `0${(timer % 60)}`.slice(-2)
-        const minutes:any = `${Math.floor(timer / 60)}`
-        const getMinutes = `0${minutes % 60}`.slice(-2)
-        const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+        const getSeconds = `0${timer % 60}`.slice(-2);
+        const minutes: any = `${Math.floor(timer / 60)}`;
+        const getMinutes = `0${minutes % 60}`.slice(-2);
+        const getHours = `0${Math.floor(timer / 3600)}`.slice(-2);
 
-        return `${getHours} : ${getMinutes} : ${getSeconds}`
-    }
+        return `${getHours} : ${getMinutes} : ${getSeconds}`;
+    };
 
     return (
-            <div>
-                <p>{formatTime()}</p>
-                    {
-                        !isActive && !isPaused ?
-                            <button onClick={handleStart}>Start</button>
-                            : (
-                                isPaused ? <button onClick={handlePause}>Pause</button> :
-                                    <button onClick={handleResume}>Resume</button>
-                            )
-                    }
-                    <button onClick={handleReset} disabled={!isActive}>Reset</button>
-            </div>
+        <div>
+            <p>{formatTime()}</p>
+            {!isActive && !isPaused ? (
+                <button onClick={handleStart}>Start</button>
+            ) : isPaused ? (
+                <button onClick={handlePause}>Pause</button>
+            ) : (
+                <button onClick={handleResume}>Resume</button>
+            )}
+            <button onClick={handleReset} disabled={!isActive}>
+                Reset
+            </button>
+        </div>
     );
 }
 
--- chess-client/src/components/chess/Board.tsx
+++ chess-client/src/components/chess/Board.tsx
@@ -1,73 +1,234 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Chessboard from "chessboardjsx";
 import { ChessInstance, ShortMove } from "chess.js";
 import { Button, Col, Container } from "react-bootstrap";
-import './Board.css'
+import "./Board.css";
 import Timer from "../Timer/Timer";
 
 const Chess = require("chess.js");
 
 const Board: React.FC = () => {
-  const [chess] = useState<ChessInstance>(
-    new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-  )
+    const [turn, setTurn] = useState("white");
+    const [chess] = useState<ChessInstance>(
+        new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    );
 
-  const [fen, setFen] = useState(chess.fen());
+    const [timeSelected, setTimeSelected] = useState(false);
 
-  const handleMove = (move: ShortMove) => {
-    if (chess.move(move)) {
-      setTimeout(() => {
-        const moves = chess.moves();
+    const [additionalTime, setAdditionalTime] = useState(0);
+    const [initialTime, setInitialTime] = useState(60);
+    const [fen, setFen] = useState(chess.fen());
 
-        if (moves.length > 0) {
-          const computerMove = moves[Math.floor(Math.random() * moves.length)];
-          chess.move(computerMove);
-          setFen(chess.fen());
+    const [timerW, setTimerW] = useState(initialTime);
+    const incrementW = useRef<any>();
+
+    const [timerB, setTimerB] = useState(initialTime);
+    const incrementB = useRef<any>();
+
+    //COMPUTER MOVES HANDLING
+    const handleMove = (move: ShortMove) => {
+        if (chess.move(move)) {
+            if (turn === "white") {
+                setTimerW(timerW + additionalTime);
+                handleBlackStart();
+                handleWhitePause();
+                console.log("white moved, black timer starts");
+                setTurn("black");
+            } else {
+                setTimerB(timerB + additionalTime);
+                setTurn("white");
+                handleWhiteStart();
+                handleBlackPause();
+                console.log("black moved, white timer starts");
+            }
+            // setTimeout(() => {
+            //     const moves = chess.moves();
+            //     if (moves.length > 0) {
+            //         const computerMove =
+            //             moves[Math.floor(Math.random() * moves.length)];
+            //         chess.move(computerMove);
+            //         setFen(chess.fen());
+            //     }
+            // }, 300);
+            setFen(chess.fen());
+            setTimeout(() => {
+                if (chess.game_over()) {
+                    if (turn === "white") {
+                        gameFinished("white");
+                    } else {
+                        gameFinished("black");
+                    }
+                }
+            }, 5);
         }
-      }, 300);
+    };
 
-      setFen(chess.fen());
+    const handleReset = () => {
+        console.log("cb");
+        chess.load("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        setFen(chess.fen());
+        handleWhiteReset();
+        handleBlackReset();
+        setTurn("white");
+        setTimeSelected(false);
+        // Timer()
+    };
+
+    const handleWhiteStart = () => {
+        incrementW.current = setInterval(() => {
+            setTimerW((timer) => timer - 1);
+        }, 1000);
+    };
+
+    const handleWhitePause = () => {
+        clearInterval(incrementW.current);
+    };
+
+    const handleWhiteReset = () => {
+        clearInterval(incrementW.current);
+        setTimerW(initialTime);
+    };
+
+    const handleBlackStart = () => {
+        incrementB.current = setInterval(() => {
+            setTimerB((timerB) => timerB - 1);
+        }, 1000);
+    };
+
+    const handleBlackPause = () => {
+        clearInterval(incrementB.current);
+    };
+
+    const handleBlackReset = () => {
+        clearInterval(incrementB.current);
+        setTimerB(initialTime);
+    };
+
+    const formatTimeW = () => {
+        const getSeconds = `0${timerW % 60}`.slice(-2);
+        const minutes: any = `${Math.floor(timerW / 60)}`;
+        const getMinutes = `0${minutes % 60}`.slice(-2);
+        const getHours = `0${Math.floor(timerW / 3600)}`.slice(-2);
+
+        if (timerW === 0) {
+            gameFinished("black");
+        }
+        return `${getHours} : ${getMinutes} : ${getSeconds}`;
+    };
+
+    const formatTimeB = () => {
+        const getSeconds = `0${timerB % 60}`.slice(-2);
+        const minutes: any = `${Math.floor(timerB / 60)}`;
+        const getMinutes = `0${minutes % 60}`.slice(-2);
+        const getHours = `0${Math.floor(timerB / 3600)}`.slice(-2);
+
+        if (timerB === 0) {
+            gameFinished("white");
+        }
+
+        return `${getHours} : ${getMinutes} : ${getSeconds}`;
+    };
+
+    const handleTimeSelect = (minutes: number, increment: number) => {
+        setTimeSelected(true);
+
+        setAdditionalTime(increment);
+
+        setTimerB(minutes * 60);
+        setTimerW(minutes * 60);
+        setInitialTime(minutes * 60);
+    };
+
+    function gameFinished(winner: string) {
+        alert(`game ended, ${winner} won!`);
+        handleBlackReset();
+        handleReset();
+        setTurn("white");
+        setTimeSelected(false);
     }
-  }
 
+    return (
+        <div>
+            <h1>Random Chess</h1>
+            <Container className="d-flex justify-content-center border shadow">
+                <div className="container">
+                    <div className="board">
+                        <Chessboard
+                            width={400}
+                            position={fen}
+                            onDrop={(move) =>
+                                handleMove({
+                                    from: move.sourceSquare,
+                                    to: move.targetSquare,
+                                    promotion: "q",
+                                })
+                            }
+                        />
+                    </div>
+                    <div className="reset">
+                        <p>{turn} to move</p>
+                        <Button onClick={handleReset}>Reiniciar</Button>
+                    </div>
+                    <div className="timer-white">
+                        <p>{formatTimeW()}</p>
 
-  const handleReset = () => {
-    console.log('cb')
-    chess.load("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    setFen(chess.fen())
+                        {/* {!isActiveW && !isPausedW ? (
+                    <button onClick={handleWhiteStart}>Start</button>
+                    ) : isPausedW ? (
+                        <button onClick={handleWhitePause}>Pause</button>
+                        ) : (
+                            <button onClick={handleWhiteResume}>Resume</button>
+                            )}
+                            <button onClick={handleWhiteReset} disabled={!isActiveW}>
+                            Reset
+                        </button> */}
+                    </div>
 
-    Timer()
+                    <div className="timer-black">
+                        <p>{formatTimeB()}</p>
 
-  }
-
-  return (
-    <Container className='d-flex justify-content-center border shadow'>
-      <Col>
-      <Timer></Timer>
-      </Col>
-      <Col>
-        <h1>Random Chess</h1>
-        <Chessboard
-          width={400}
-          position={fen}
-          onDrop={(move) =>
-            handleMove({
-              from: move.sourceSquare,
-              to: move.targetSquare,
-              promotion: "q",
-            })
-          }
-        />
-
-      </Col>
-      <Col>
-        <Container className='rounded mt-5 h-75' id='column2Conteiner'>
-        <Button onClick={handleReset}>Reiniciar</Button>
-
-        </Container>
-      </Col>
-    </Container>
-  );
+                        {/* {!isActiveB && !isPausedB ? (
+                    <button onClick={handleBlackStart}>Start</button>
+                    ) : isPausedB ? (
+                        <button onClick={handleBlackPause}>Pause</button>
+                        ) : (
+                            <button onClick={handleBlackResume}>Resume</button>
+                            )}
+                            <button onClick={handleBlackReset} disabled={!isActiveB}>
+                            Reset
+                        </button> */}
+                    </div>
+                    <div className="buttons">
+                        {timeSelected ? null : (
+                            <div>
+                                <Button
+                                    onClick={() => {
+                                        handleTimeSelect(1, 1);
+                                    }}
+                                >
+                                    1 | 1
+                                </Button>
+                                <Button
+                                    onClick={() => {
+                                        handleTimeSelect(5, 5);
+                                    }}
+                                >
+                                    5 | 5
+                                </Button>
+                                <Button
+                                    onClick={() => {
+                                        handleTimeSelect(10, 0);
+                                    }}
+                                >
+                                    10
+                                </Button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
 };
 
 export default Board;
